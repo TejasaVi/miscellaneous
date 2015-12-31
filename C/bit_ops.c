2a9514e0447555a999244e5e732e933fac0aaeda
Tejas Vaykole <tejas.vaykole@calsoftinc.com>
2015-12-31 13:17:41 +0530

    Adding the sample bitOperations in C.
        int abs(int):               Compute integer absolute value without branching.
        void swap(int*,int*):       Swap two numbers without temporary variable.
        int findMinimumof(int,int): Find minimum value from two integers.Without if-else.
        int findMaximumof(int,int): Find maximum value from two integers.Without if-else.
        int maskLS1(int):           Masking the Least significant 1bit from a number.
        int set_flagbit(int,int):   Set a bit in flag at given position.
        int clearflagbit(int,int):  Clear a bit in flag at given position.
        int toggle_flagbit(int,int):Toggle a bit in flag at given position.
        int check_bitset(int,int):  Check if the given position bit is set.

---
+++ C/bit_ops.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<limits.h>
+
+/*
+Compute integer absolute value without branching
+*/
+int abs(int number)
+{
+    int const mask = number >> sizeof(int)* CHAR_BIT -1;
+    return (number^mask) - mask;
+}
+
+
+/*
+Swap two numbers without temporary variable.
+*/
+void swap(int* first, int* second)
+{
+    *first = *first^*second;
+    *second= *first^*second;
+    *first = *first^*second;
+    return;
+}
+
+/*
+Find minimum value from two integers.Without if-else.
+*/
+int findMinimumof(int first, int second){
+    return second^((first^second)&-(first<second));
+}
+
+/*
+Find maximum value from two integers.Without if-else.
+*/
+int findMaximumof(int first, int second){
+    return second^((first^second)&-(first>second));
+}
+
+/*
+Masking the Least significant 1bit from a number.
+*/
+int maskLS1(int number)
+{
+    return (number & ( -number ));
+}
+
+/*
+Set a bit in flag at given position.
+*/
+int set_flagbit(int flag, int bitnum)
+{
+    return flag |= 1 << bitnum;
+}
+
+
+/*
+Clear a bit in flag at given position.
+*/
+int clear_flagbit(int flag, int bitnum)
+{
+    return flag &= ~(1 << bitnum);
+}
+
+
+/*
+Toggle a bit in flag at given position.
+*/
+int toggle_flagbit(int flag, int bitnum)
+{
+    return flag ^= 1 << bitnum;
+}
+
+/*
+Check if the given position bit is set.
+*/
+int check_bitset(int flag, int bitnum)
+{
+    return (flag>>bitnum)& 1;
+}
+
