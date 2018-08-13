7fb020ecfdbcc383a1a0f37861df39678d286428
Tejas Vaykole <tejas.vaykole@emc.com>
2018-08-13 14:46:33 +0530

    Added if-else block checker tool

---
+++ python/if-else-checker/testfile.py
@@ -0,0 +1,22 @@
+num = float(input("Enter a number: "))
+
+if num >= 0:
+    #if True: print "True " \ else: print "False"
+
+    try:
+        pass
+    except:
+        pass
+
+    if num == 0:
+        print "zero"
+    elif True:
+        print "Non-zero number"
+    else:
+        print "Test"
+#else:
+    print "Negative Number"
+    if num == 0:
+        print "zero"
+    else:
+        print "Non-zero number"
