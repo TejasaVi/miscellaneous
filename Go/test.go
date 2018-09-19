1307ba591b5a24228ef0baee43464cc45c371f85
Tejas Vaykole <tejas.vaykole@emc.com>
2018-09-19 14:40:52 +0530

    Added Go directory

---
+++ Go/test.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int = 10
+	const LEN int = 20
+	fmt.Println(LEN)
+	var str string = "This is a String\n"
+	fmt.Println("Variable X has Vaule =", x)
+	fmt.Println("Hello WOrld!!!")
+	fmt.Println(str)
+}
