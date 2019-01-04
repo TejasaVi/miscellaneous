0395e157a3e14b8cde6bf74af41e535b42227450
Tejas Vaykole <xxx>
2019-01-04 15:28:59 -0500

    seperated git_push func

--- python/scraper.py
+++ python/scraper.py
@@ -34,10 +34,21 @@ def scrape_page(url_link):
 
 def clone_git_repo(list_item):
 	try:
-		repo = git.Repo.clone_from(list_item.url, list_item.name)
-		repo.git.checkout("-b", list_item.branch)
+		repo = git.Repo.clone_from(str(list_item['url']), str(list_item['name']))
+		repo.git.checkout("-b", str(list_item['branch']))
 	except Exception as e:
 		print (e)
+
+def git_push_remote(origin='origin', branch=None):
+    try:
+        # push changes to remote repo
+        origin = repo.remote(origin)
+        if branch is not None:
+            origin.push(branch, set_upstream=True)
+        else:
+            origin.push()
+    except Exception as e:
+        print (e)
 
 if __name__ == "__main__":
 	# Scrape page
@@ -46,11 +57,11 @@ if __name__ == "__main__":
 	listings = scrape_page(access_url)
 	#print (listings)
 	print("Webpage scraping: Complete !!")
-	
+
 	get_file(file_url, file_to_copy)
 	print("Changed file download: Complete !!")
 
-	
+
 	#FOR EACH item in List DO 
 	for item in listings:
 		# Clone Repo
@@ -60,19 +71,16 @@ if __name__ == "__main__":
 
 		# Move file to local Repo
 		print("Copying changes to the local Repository....")
-		shutil.copyfile(file_to_copy, os.path.join(item.name, "src/", "index.js"))
+		shutil.copyfile(file_to_copy, os.path.join(item['name'], "src/", "index.js"))
 		print("Copying changes to the local Repository: Complete !!")
 		# Add file to local_repo
-		repo_dir = "my_dir"
-		repo = git.Repo(repo_dir)
-        file_to_copy = [item.a_path for item in repo.index.diff(None)]
-        repo.index.add(file_to_copy)
+		repo = git.Repo(str(item['name']))
+                file_to_copy = [itm.a_path for itm in repo.index.diff(None)]
+                repo.index.add(file_to_copy)
 		print("Adding changed files to local Repository: Complete !!")
 		# commit files to local_repo
 		commit_message = "Commit Message"
 		repo.index.commit(commit_message)
 		print("Commit changes to local Repository: Complete !!")
-        # push changes to remote repo
-        origin = repo.remote("origin")
-		origin.push("origin","crowdstrike1" )
+                #git_push_remote(branch=str(item['branch']))
 		print("Pushing changed files to Remote Repository: Complete !!")
