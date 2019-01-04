d4e2022ab6a67d7b73188d3590d4b97b62df7149
Tejas Vaykole <tejas.vaykole91@gmail.com>
2019-01-05 00:44:57 +0530

    minor changes  with object

--- python/scraper.py
+++ python/scraper.py
@@ -32,15 +32,12 @@ def scrape_page(url_link):
     listings.append(repo_details)
   return listings
 
-def clone_git_repo():
-	repo_clone_url = "https://bitbucket.org/zeeshan_07/cs1/src/master/"
-	local_repo = "my_dir"
-	test_branch = "crowdstrike1"
-	repo = git.Repo.clone_from(repo_clone_url, local_repo)
-	repo.git.checkout("-b", test_branch)
-        os.system("ls ./my_dir")
-	#shutil.rmtree("my_dir")
-
+def clone_git_repo(list_item):
+	try:
+		repo = git.Repo.clone_from(list_item.url, list_item.name)
+		repo.git.checkout("-b", list_item.branch)
+	except Exception as e:
+		print (e)
 
 if __name__ == "__main__":
 	# Scrape page
@@ -49,29 +46,33 @@ if __name__ == "__main__":
 	listings = scrape_page(access_url)
 	#print (listings)
 	print("Webpage scraping: Complete !!")
-	# Get File
+	
 	get_file(file_url, file_to_copy)
 	print("Changed file download: Complete !!")
-	# Clone Repo
-	print("Cloning Remote Repository....")
-	clone_git_repo()
-	print("Remote Repository: Complete!!")
 
-	# Move file to local Repo
-	print("Copying changes to the local Repository....")
-	shutil.copyfile(file_to_copy, os.path.join("my_dir/src/", "index.js"))
-	print("Copying changes to the local Repository: Complete !!")
-	# Add file to local_repo
-	repo_dir = "my_dir"
-	repo = git.Repo(repo_dir)
+	
+	#FOR EACH item in List DO 
+	for item in listings:
+		# Clone Repo
+		print("Cloning Remote Repository....")
+		clone_git_repo(item)
+		print("Remote Repository: Complete!!")
+
+		# Move file to local Repo
+		print("Copying changes to the local Repository....")
+		shutil.copyfile(file_to_copy, os.path.join(item.name, "src/", "index.js"))
+		print("Copying changes to the local Repository: Complete !!")
+		# Add file to local_repo
+		repo_dir = "my_dir"
+		repo = git.Repo(repo_dir)
         file_to_copy = [item.a_path for item in repo.index.diff(None)]
         repo.index.add(file_to_copy)
-	print("Adding changed files to local Repository: Complete !!")
-	# commit files to local_repo
-	commit_message = "Commit Message"
-	repo.index.commit(commit_message)
-	print("Commit changes to local Repository: Complete !!")
+		print("Adding changed files to local Repository: Complete !!")
+		# commit files to local_repo
+		commit_message = "Commit Message"
+		repo.index.commit(commit_message)
+		print("Commit changes to local Repository: Complete !!")
         # push changes to remote repo
         origin = repo.remote("origin")
-	origin.push("origin","crowdstrike1" )
-	print("Pushing changed files to Remote Repository: Complete !!")
+		origin.push("origin","crowdstrike1" )
+		print("Pushing changed files to Remote Repository: Complete !!")
