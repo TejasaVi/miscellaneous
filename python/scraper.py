2d42fbbc7fae4bd506cab9fe18ff3fc1aa9fd43c
Tejas Vaykole <xxx>
2019-01-04 17:01:48 -0500

    fixed the coloumn issue

--- python/scraper.py
+++ python/scraper.py
@@ -21,16 +21,18 @@ def get_file(url, name):
 		print (e)
 
 def scrape_page(url_link):
-  response = requests.get(url_link)
-  soup = BeautifulSoup(response.text, "html.parser")
-  listings = [];repo_details = {} 
-  for rows in soup.find_all("tr"):
-    for row in rows.find_all("td"): 
-      repo_details["name"] = rows.find_all("td")[0].get_text()
-      repo_details["url"] = rows.find_all("td")[1].get_text()
-      repo_details["branch"] = rows.find_all("td")[2].get_text()
-    listings.append(repo_details)
-  return listings
+    response = requests.get(url_link)
+    soup = BeautifulSoup(response.text, "html.parser")
+    listings = [];repo_details = {}
+    data_rows = len(soup.find_all("tr")) - 1
+    for row in range(0, data_rows) :
+        col = 3*row
+        repo_details['name'] = soup.find_all("td")[col ].get_text()
+        repo_details['url'] = soup.find_all("td")[col +1 ].get_text()
+        repo_details['branch'] = soup.find_all("td")[col + 2].get_text()
+        listings.append(repo_details)
+        repo_details = {}
+    return listings
 
 def clone_git_repo(list_item):
 	try:
@@ -53,16 +55,15 @@ def git_push_remote(origin='origin', branch=None):
 if __name__ == "__main__":
 	# Scrape page
 	print("Starting Webpage scraping....")
-	scrape_page(access_url)
 	listings = scrape_page(access_url)
-	#print (listings)
+	print (listings)
 	print("Webpage scraping: Complete !!")
 
 	get_file(file_url, file_to_copy)
 	print("Changed file download: Complete !!")
 
 
-	#FOR EACH item in List DO 
+	#FOR EACH item in List DO
 	for item in listings:
 		# Clone Repo
 		print("Cloning Remote Repository....")
@@ -75,8 +76,8 @@ if __name__ == "__main__":
 		print("Copying changes to the local Repository: Complete !!")
 		# Add file to local_repo
 		repo = git.Repo(str(item['name']))
-                file_to_copy = [itm.a_path for itm in repo.index.diff(None)]
-                repo.index.add(file_to_copy)
+                file_path = [itm.a_path for itm in repo.index.diff(None)]
+                repo.index.add(file_path)
 		print("Adding changed files to local Repository: Complete !!")
 		# commit files to local_repo
 		commit_message = "Commit Message"
@@ -84,3 +85,5 @@ if __name__ == "__main__":
 		print("Commit changes to local Repository: Complete !!")
                 #git_push_remote(branch=str(item['branch']))
 		print("Pushing changed files to Remote Repository: Complete !!")
+                commit_id = repo.head.commit
+                print ("Commit ID: {0}", commit_id)
